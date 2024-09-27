0af6b335f1089c0acdf1cd0f584d145596280096
Nivahs <nivahs.lima13@hotmail.com>
2024-09-27 18:09:53 -0300

    utilizing-switch

--- Program.cs
+++ Program.cs
@@ -1,18 +1,76 @@
-﻿for (int i = 0 + 1; i <= 100; i++)
-{
-    if (i % 3 == 0 && i % 5 == 0){
-        Console.WriteLine($"{i} - FizzBuzz");
-    }
+﻿//Random coin = new ();
 
-    else if (i % 3 == 0){
-      Console.WriteLine($"{i} - Fizz");
-   }
-    else if (i % 5 == 0)
-    {
-        Console.WriteLine($"{i} - Buzz");
-    }
-    
-    else{   
-        Console.WriteLine(i);
-    }  
+//int side = coin.Next(0, 2);
+
+//Console.WriteLine($"Result: {(side == 1 ? "Head" : "Tails")}");
+
+
+
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+using System.Collections;
+using System.Diagnostics;
+
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+    case "01":
+    type = "Sweat shirt";
+    break;
+
+    case "02":
+    type = "T-Shirt";
+    break;
+
+    case "03":
+    type = "Sweat pants";
+    break;
+
+    default:
+        type = "Other";
+        break;
 }
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+
+    case "MN":
+        color = "Maroon";
+        break;
+    
+    default:
+        color = "White";
+        break;
+}
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    
+    case "M":
+        size = "Medium";
+        break;
+    
+    case "L":
+        size = "Larga";
+        break;
+    
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+
+
